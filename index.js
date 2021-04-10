5e1502490fe486c39c603d5813c9ee76d54119a7
Andrea Pietronave <andrea.pietronave.97@gmail.com>
2021-04-10 11:45:26 +0200

    Updated Titles

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const http = require("http").Server(app);
 const port = process.env.PORT || 3000;
 app.use(favicon(path.join(__dirname,'public','images','icon.png')));
 app.use(express.static(path.join(__dirname, "public"), { extensions: ["html"] }));
+app.use(function (req, res) {
+    res.status(404).send("Sorry can't find that!");
+});
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 http.listen(port);
 console.log(`Listening on port ${port}!`);
